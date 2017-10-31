1ef4102704d04538fd378fda21e880c7934062b5
Cambrai1 <32384758+Cambrai1@users.noreply.github.com>
2017-10-31 13:06:47 +0000

    restore

---
+++ New Unity Project/New Unity Project/Assets/Scripts/CharBehaviour.cs
@@ -0,0 +1,74 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace ISS
+{
+    public class CharBehaviour : MonoBehaviour
+    {
+        [SerializeField]
+        private float m_speed = 10.0f;
+
+        [SerializeField]
+        private float m_rotationSpeed = 50.0f;
+
+        [SerializeField]
+        private AudioSource audioSource;
+
+        private Transform m_transform;
+        public Rigidbody rb;
+        public float speed = 10.0f;
+        public int moveSpeed = 10;
+        // Use this for initialization
+
+        void Start()
+        {
+            m_transform = GetComponent<Transform>();
+            rb = GetComponent<Rigidbody>();
+
+            audioSource = GetComponent<AudioSource>();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+            float rotY = m_transform.rotation.eulerAngles.y;
+
+            m_transform.rotation = Quaternion.Euler(new Vector3(0.0f, rotY + m_rotationSpeed *
+                Input.GetAxis("Horizontal") * Time.deltaTime, 0.0f));
+
+            m_transform.position += m_transform.forward * m_speed * Input.GetAxis("Vertical") * Time.deltaTime;
+
+            if (Input.GetKeyDown("q"))
+                rb.velocity = new Vector3(rb.velocity.x, 10, rb.velocity.z);
+
+            if (Input.GetKey("w"))
+            {
+                
+                audioSource.Play();
+            }
+            else
+            {
+                audioSource.Pause();
+            }
+            //if (Input.GetKeyDown("w"))
+            //rb.velocity = new Vector3(5, rb.velocity.y, rb.velocity.z);
+            //if (Input.GetKey(KeyCode.W))
+            //{
+            //transform.position += (Vector3.forward * moveSpeed * Time.deltaTime);
+            //}
+            //if (Input.GetKey(KeyCode.S))
+            //{
+            //transform.position += (Vector3.back * moveSpeed * Time.deltaTime);
+            //}
+            //if (Input.GetKeyDown("a"))
+            //rb.velocity = new Vector3(rb.velocity.x, rb.velocity.y, 5);
+
+            //if (Input.GetKeyDown("d"))
+            // rb.velocity = new Vector3(rb.velocity.x, rb.velocity.y, -5);
+
+        }
+
+    }
+
+}
---
+++ New Unity Project/New Unity Project/Assets/Scripts/DoorManagement.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorManagement : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+    }
+
+    [SerializeField]
+    private Animator door1;
+
+    [SerializeField]
+    private Animator door2;
+
+    private bool isOpen = false;
+
+    // Update is called once per frame
+    void Update () {
+	    if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (isOpen)
+            {
+                door1.SetTrigger("Close Doors");
+                door2.SetTrigger("Close Doors");
+            }
+            else
+            {
+                door1.SetTrigger("Open Doors");
+                door2.SetTrigger("Open Doors");
+            }
+
+            isOpen = !isOpen;
+        }
+       
+        
+	}
+}
