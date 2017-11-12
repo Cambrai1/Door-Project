c9a9a4ff780beae3f1811d8fd225ef857b559d5f
Cambrai1 <32384758+Cambrai1@users.noreply.github.com>
2017-11-12 17:05:38 +0000

    Added a door on the other side of the corridor

---
+++ New Unity Project/Assets/Scripts/CamController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ISS
+{
+    public class CamController : MonoBehaviour
+    {
+
+        [SerializeField]
+        private float m_speed = 3.0f;
+
+        [SerializeField]
+        private float m_rotationSpeed = 100.0f;
+
+        [SerializeField]
+        private Transform m_player;
+
+        private Transform m_transform;
+        public Rigidbody rb;
+
+        // Use this for initialization
+        void Start()
+        {
+            m_transform = GetComponent<Transform>();
+            rb = GetComponent<Rigidbody>();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            float rotY = m_player.rotation.eulerAngles.y;
+
+            var distance = Vector3.Distance(m_transform.position, m_player.position);
+
+            if (Input.GetKey("left"))
+            {
+                m_player.rotation = Quaternion.Euler(new Vector3(0.0f, rotY - m_rotationSpeed * Time.deltaTime, 0.0f));
+            }
+
+            if (Input.GetKey("right"))
+            {
+                m_player.rotation = Quaternion.Euler(new Vector3(0.0f, rotY + m_rotationSpeed * Time.deltaTime, 0.0f));
+            }
+        }
+    }
+}
